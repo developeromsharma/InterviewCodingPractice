faa288ccd4c936412117b7b14e6ec1bd5d64d53e
OM SHARMA <developeromsharma@gmail.com>
2025-07-17 19:52:52 +0530

    added sql query

---
+++ InterviewCodingPractice/SQL/SQLInterviewQuestions/SQLInterviewQuestions/Queries/SecondHighestSalary.sql
@@ -0,0 +1,22 @@
+﻿/*SQL QUERY */
+
+Select top 1*
+from
+(
+	select top 2 salary
+	employess
+	order by salary desc
+)
+as innerQuery
+order by salary asc;
+
+/*LINQ QUERY 
+
+var secondHighestSalary = employees
+	.Select(e => e.Salary)
+	.Distinct()
+	.OrderByDescending(s => s)
+	.Skip(1)
+	.FirstOrDefault();
+
+	*/
