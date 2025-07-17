7d7d221499a374f367fd127e6906acbdea8d4348
OM SHARMA <developeromsharma@gmail.com>
2025-07-17 19:35:53 +0530

    added code for interview question on 17/07/2025

---
+++ InterviewCodingPractice/PractiseSets/PractiseQuestions/PractiseQuestions/PrintElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    //static void Main()
+    //{
+    //    PrintNumbers(1);
+    //}
+
+    static void PrintNumbers(int num)
+    {
+        if (num > 10)
+            return;
+
+        Console.WriteLine(num);
+        PrintNumbers(num + 1);
+    }
+}
---
+++ InterviewCodingPractice/PractiseSets/PractiseQuestions/PractiseQuestions/SecondHighestElement.cs
@@ -0,0 +1,35 @@
+﻿namespace PractiseQuestions
+{
+    class SecondHighestElement
+    {
+        static void Main()
+        {
+            int[] arr = { 12, 5, 7, 9, 15, 6, 20 };
+
+            int first = int.MinValue;
+            int second = int.MinValue;
+
+            foreach (int num in arr)
+            {
+                if (num > first)
+                {
+                    second = first;
+                    first = num;
+                }
+                else if (num > second && num != first)
+                {
+                    second = num;
+                }
+            }
+
+            if (second == int.MinValue)
+            {
+                Console.WriteLine("No second largest element found.");
+            }
+            else
+            {
+                Console.WriteLine("Second largest element is: " + second);
+            }
+        }
+    }
+}
